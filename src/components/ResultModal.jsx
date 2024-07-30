9b8a81088fff8aeaf1b097ea0d0e4927090d582e
minsik90 <minsik90@icloud.com>
2024-07-30 10:04:23 +0900

    Exposing Component API with the `useImperativeHandle` Hook

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,17 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog ref={ref} className="result-modal">
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}won : Lost</h2>
       <p>
         The target Time was <strong>{targetTime} seconds.</strong>
